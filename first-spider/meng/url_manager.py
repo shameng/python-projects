5915e45af24ae1eeb05e6ce6beac12264449d4f2
imagedt_xindemeng <dingbang@imagedt.com>
2018-02-24 19:06:44 +0800

    *

--- first-spider/meng/url_manager.py
+++ first-spider/meng/url_manager.py
@@ -4,3 +4,26 @@
 @author: xindemeng
 @time: 2018/2/24 18:53
 """
+class UrlManager(object):
+
+    def __init__(self):
+        self.new_urls = set()#未爬取的URL集合
+        self.old_urls = set()#已爬取的URL集合
+
+    def has_new_url(self):
+        '''
+        判断是否有未爬取的URL
+        :return:
+        '''
+        return self.new_url_size() != 0
+
+    def new_url_size(self):
+        return len(self.new_urls)
+
+    def old_url_size(self):
+        return len(self.old_urls)
+
+    def get_new_url(self):
+        new_url = self.new_urls.pop()
+        self.old_urls.add(new_url)
+        return new_url
